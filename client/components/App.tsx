5f6520e928915f2f950f10defb6c62f8b9b14ab7
Tori Stevens <tori.stevens13@gmail.com>
2023-10-19 15:29:46 +1300

    feat: initial layouts started

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,13 +1,37 @@
-import { useFruits } from '../hooks/useFruits.ts'
+// import { useFruits } from '../hooks/useFruits.ts'
 
 function App() {
-  const { data } = useFruits()
+  // const { data } = useFruits()
 
   return (
     <>
       <div className="app">
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul>
+        <a href="/">
+          <h1>Tori Stevens Design</h1>
+        </a>
+        <h3>Choose your adventure</h3>
+        <div className="categories">
+          <div className="web-dev">
+            <a href="/web-design">
+              <img src="../client/images/web.png" alt="Web Dev" />
+            </a>
+          </div>
+          <div className="graphic-design">
+            <a href="/graphic-design">
+              <img src="../client/images/graphic.png" alt="Graphic Design" />
+            </a>
+          </div>
+          <div className="photography">
+            <a href="/photography">
+              <img src="../client/images/photog.png" alt="Photography" />
+            </a>
+          </div>
+          <div className="illustrations">
+            <a href="illustrations">
+              <img src="../client/images/illust.png" alt="Illustrations" />
+            </a>
+          </div>
+        </div>
       </div>
     </>
   )
