cb9e013a419cd701db8f547b506279ce44267c19
Tori Stevens <tori.stevens13@gmail.com>
2024-01-11 10:08:00 +1300

    getting back into it

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -10,30 +10,46 @@ function App() {
           <h1>Tori Stevens Design</h1>
         </a>
         <h3>Choose your adventure</h3>
+        {/* could do as table OR could do as responsive   display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(25rem, 1fr));
+  gap: 0.125rem;
+  margin-top: 20px; */}
         <div className="categories">
-          <div className="web-dev">
-            <a href="/web-design">
-              <img src="../client/images/web.png" alt="Web Dev" />
-            </a>
-          </div>
-          <div className="graphic-design">
-            <a href="/graphic-design">
-              <img
-                src="../client/images/graphic-colour.png"
-                alt="Graphic Design"
-              />
-            </a>
-          </div>
-          <div className="photography">
-            <a href="/photography">
-              <img src="../client/images/photog.png" alt="Photography" />
-            </a>
-          </div>
-          <div className="illustrations">
-            <a href="illustrations">
-              <img src="../client/images/illust.png" alt="Illustrations" />
-            </a>
-          </div>
+          <table id="initial-choice">
+            <tr>
+              <td>
+                <div className="web-dev">
+                  <a href="/web-design">
+                    <img src="/g" alt="Web Dev" />
+                  </a>
+                </div>
+              </td>
+              <td>
+                <div className="graphic-design">
+                  <a href="/graphic-design">
+                    <img src="/" alt="Graphic Design" />
+                  </a>
+                </div>
+              </td>
+              <td>
+                <div className="photography">
+                  <a href="/photography">
+                    <img
+                      src="../../client/images/photog-colour.png"
+                      alt="Photography"
+                    />
+                  </a>
+                </div>
+              </td>
+              <td>
+                <div className="illustrations">
+                  <a href="illustrations">
+                    <img src="/" alt="Illustrations" />
+                  </a>
+                </div>
+              </td>
+            </tr>
+          </table>
         </div>
       </div>
     </>
